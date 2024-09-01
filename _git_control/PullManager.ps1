4fb27600ce213e7cdf03ef72a6220d06d3a6c526
wizzense <wizzense@wizzense.com>
2024-09-01 13:28:05 -0700

    updated readme

--- _git_control/PullManager.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [string]$BranchName = "my-new-branch",
-    [string]$PRTitle = "Default PR Title",
-    [string]$PRBody = "Description of the changes."
-)
-
-# Create a pull request
-gh pr create --title "$PRTitle" --body "$PRBody" --head "$BranchName" --base main
-Write-Host "Pull request created for branch '$BranchName'."
